b6faf5d87874f90b095e979b975212f8216aeb41
surenbandara7 <surenbandara7@gmail.com>
2024-04-30 06:46:27 +0530

    Uncommented debugging codes

--- app/src/main/java/com/krakendepp/heart_beat/BTCommunication/BTDataHandler.java
+++ app/src/main/java/com/krakendepp/heart_beat/BTCommunication/BTDataHandler.java
@@ -77,55 +77,9 @@ public class BTDataHandler extends AsyncTask<Void, Void, String> {
 
 
             // Update UI components here if needed
-            onProgressUpdate(-1);
-            System.out.println("All good up to now");
-
-
-                for (int i = 0; i < 100; i++) {
-                    onProgressUpdate(50);
-                    try {
-                        Thread.sleep(50); // 50 milliseconds
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-
-
-                while (true) {
-                    int finalValue = (int) (Math.random() * 101);
-                    if (finalValue == 1000){
-                        break;
-                    }
-                    activity.runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            heartRate.setText(String.valueOf(finalValue)+" bpm");}});
-
-                    onProgressUpdate(finalValue);
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        System.out.println("Problem in sleep ");
-                    }
-                }
-
-
-
-        return "Ok";
-//        BluetoothSocket bluetoothSocket= btConnector.connectToDevice(bluetoothDevice);
-//        if (bluetoothSocket != null && bluetoothSocket.isConnected()) {
-//            // Update UI components here if needed
 //            onProgressUpdate(-1);
 //            System.out.println("All good up to now");
-//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {}
 //
-//
-//            try {
-//                InputStream inputStream = bluetoothSocket.getInputStream();
-//                String constructor = "";
-//                int value;
 //
 //                for (int i = 0; i < 100; i++) {
 //                    onProgressUpdate(50);
@@ -135,39 +89,85 @@ public class BTDataHandler extends AsyncTask<Void, Void, String> {
 //                        e.printStackTrace();
 //                    }
 //                }
+//
+//
+//
 //                while (true) {
-//                    byte b = (byte) inputStream.read();
-//                    if (b != 13 && b != 10) {
-//                        constructor = constructor + (char) b;
-//                    } else {
-//                        if (b == 13) {
+//                    int finalValue = (int) (Math.random() * 101);
+//                    if (finalValue == 1000){
+//                        break;
+//                    }
+//                    activity.runOnUiThread(new Runnable() {
 //
-//                            try{
-//                            value = Integer.parseInt(constructor);
-//                                int finalValue = value;
-//                                activity.runOnUiThread(new Runnable() {
-//
-//                                    @Override
-//                                    public void run() {
+//                        @Override
+//                        public void run() {
 //                            heartRate.setText(String.valueOf(finalValue)+" bpm");}});
-//                            constructor = "";
-//                            onProgressUpdate(value);}
-//                            catch (Exception e){
-//                                System.out.println(e);
-//                            }
-//                        }
+//
+//                    onProgressUpdate(finalValue);
+//                    try {
+//                        Thread.sleep(50);
+//                    } catch (InterruptedException e) {
+//                        System.out.println("Problem in sleep ");
 //                    }
 //                }
-//            } catch (IOException e) {
-//                onProgressUpdate(-2);
-//            }
-//        } else {
-//            // Update UI components here if needed
-//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {}
 //
-//        }
+//
 //
 //        return "Ok";
+        BluetoothSocket bluetoothSocket= btConnector.connectToDevice(bluetoothDevice);
+        if (bluetoothSocket != null && bluetoothSocket.isConnected()) {
+            // Update UI components here if needed
+            onProgressUpdate(-1);
+            System.out.println("All good up to now");
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {}
+
+
+            try {
+                InputStream inputStream = bluetoothSocket.getInputStream();
+                String constructor = "";
+                int value;
+
+                for (int i = 0; i < 100; i++) {
+                    onProgressUpdate(50);
+                    try {
+                        Thread.sleep(50); // 50 milliseconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                while (true) {
+                    byte b = (byte) inputStream.read();
+                    if (b != 13 && b != 10) {
+                        constructor = constructor + (char) b;
+                    } else {
+                        if (b == 13) {
+
+                            try{
+                            value = Integer.parseInt(constructor);
+                                int finalValue = value;
+                                activity.runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                            heartRate.setText(String.valueOf(finalValue)+" bpm");}});
+                            constructor = "";
+                            onProgressUpdate(value);}
+                            catch (Exception e){
+                                System.out.println(e);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                onProgressUpdate(-2);
+            }
+        } else {
+            // Update UI components here if needed
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {}
+
+        }
+
+        return "Ok";
 
     }
 
